bb7a14131ca1dd40ddb28985fbb58d763ab95e19
SdazaP <creepersdp57@gmail.com>
2025-05-14 18:24:29 -0600

    Inicio de proyecto

---
+++ proyblog/proyblog/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for proyblog project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'proyblog.settings')
+
+application = get_asgi_application()
