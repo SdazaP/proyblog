9d49287e7a948d93b285a3e4262613f0fd599948
SdazaP <creepersdp57@gmail.com>
2025-05-22 20:55:06 -0600

    Post y URL del blog

--- proyblog/blog/models.py
+++ proyblog/blog/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class Post(models.Model):
+    titulo = models.CharField(max_length=200)
+    contenido = models.TextField()
+    fecha = models.DateTimeField(default=timezone.now)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+
+
+
